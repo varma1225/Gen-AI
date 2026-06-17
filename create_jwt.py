8d6671c378d4c63c89c97fa37918236bb57b15a3
varma <varma08.asa@gmail.com>
2026-06-17 21:16:27 +0530

    Added create_jwt.py

---
+++ create_jwt.py
@@ -0,0 +1,24 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key_123")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
